698b7c1fcd2745bf87ac918105a21d1ab9592f73
Anna <zharova1992@gmail.com>
2022-07-26 23:53:11 +0400

    задачи 64, 66

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 64
+
+/* int count(int n)
+{
+    if (n / 10 != 0)
+        return 1 + count(n / 10);
+    else
+        return 1;
+}
+
+Console.WriteLine("Введите число, в котором посчитаем количество цифр: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Количество цифр: ");
+Console.WriteLine(count(n)); */
+
+// Задача 66
+
+/* Console.WriteLine("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = m;
+
+if (m > n)
+{
+    m = n;
+    n = count;
+}
+
+NatSumm(m, n, count = 0);
+
+void NatSumm(int m, int n, int summ)
+{
+    summ = summ + n;
+    if (n <= m)
+    {
+        Console.Write($"Сумма натуральных элементов: {summ}");
+        return;
+    }
+    NatSumm(m, n - 1, summ);
+} */
